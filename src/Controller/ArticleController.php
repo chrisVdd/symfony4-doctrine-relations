aab87b269ab3cdd98399d54a982df634c65a85d7
manueljun17 <manueljun17@gmail.com>
2019-06-14 16:38:54 +0800

    Fetching Relations

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommentRepository;
 
 class ArticleController extends AbstractController
 {
@@ -44,14 +45,10 @@ class ArticleController extends AbstractController
         if ($article->getSlug() === 'khaaaaaan') {
             $slack->sendMessage('Kahn', 'Ah, Kirk, my old friend...');
         }
-        $comments = [
-            'I ate a normal rock once. It did NOT taste like bacon!',
-            'Woohoo! I\'m going on an all-asteroid diet!',
-            'I like bacon too! Buy some from my site! bakinsomebacon.com',
-        ];
+        
+       
         return $this->render('article/show.html.twig', [
             'article' => $article,
-            'comments' => $comments,
         ]);
     }
 
